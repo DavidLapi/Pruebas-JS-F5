d577d06b9d4d9c9a1dd9856d838214aeb8f59beb
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-07-08 21:49:23 +0200

    Update ejemplo3.js

---
+++ ejemplo3.js
@@ -0,0 +1,30 @@
+// Funciones predefinidas
+/* JavaScript incluye una serie de funciones ya definidas que pueden ser usadas 
+con cualquier variable, valor directo u objeto. Gracias a estas funciones
+podemos realizar una conversión exxplícita de tipos de datos*/
+
+// Funcion isNaN --> Comprueba si un valor no es un número, por lo que devolverá 
+// un booleano negativo (false) en caso de ser un número y un booleano positivo
+// (true) en cualquier otro caso.
+
+var num;
+
+// Funcion isFinite --> Comprueba si un valor es un número finito. Si el valor
+// equivale a undefined, NaN, Infinity o -Infinity se devolverá false y en el 
+// resto de los casos se devolverá true.
+
+
+
+// Funcion parseInt --> Permite intentar transformar cualquier valor en un número
+// entero (int). 
+
+
+
+// Función parseFloat --> Permite convertir cualquier valor, interpretado como cadena,
+// en un número real siguiendo las mismas reglas que parseInt.
+
+
+
+// Funcion eval --> Interpreta una cadena y la ejecuta como si se tratase de una porción
+// de código de JavaScript, devolviendo el resultado de dicha ejecución.
+
