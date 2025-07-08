0b7d758d6676a8703249932e00e8a4b03470eddf
DavidLapi <davidvaamonde.bueno@gmail.com>
2025-07-08 19:59:05 +0200

    Documentado 1

---
+++ ejemplo1.js
@@ -0,0 +1,13 @@
+function sumar() {
+    var miSuma = 9 + 12;
+    return console.log(miSuma);
+}
+
+function resta() {
+    var miResta = 4 - 9;
+    return miResta;
+}
+
+sumar()
+console.log(resta())
+
